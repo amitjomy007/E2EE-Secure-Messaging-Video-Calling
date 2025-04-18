6dcd0f19ad6b314e556547efc348d3c3214fce16
amitjomy007 <amitjomy007@gmail.com>
2025-04-18 12:08:36 +0530

    init\socket initialized

---
+++ apps/server/src/services/socket.ts
@@ -0,0 +1,28 @@
+import {Server, Socket} from "socket.io"
+
+class SocketService {
+    private _io: Server;
+
+    constructor() {
+        console.log("initializing a new Server")
+        this._io = new Server();
+        console.log('Initialized a new Server');
+    }
+
+    public initListeners() {
+        const io = this._io;
+        console.log("INitializing socket lsiteners>...");
+        io.on('connect', (socket) => {
+            console.log(`new socket with id : ${socket.id} has connected`);
+            socket.on('event:message', async ({message} : {message:string})=>{
+                console.log("New message : " , message);
+            })
+        })
+    }
+    get io(){
+        return this._io;
+
+    }
+}
+
+export default SocketService;
